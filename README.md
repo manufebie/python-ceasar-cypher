7eb43e6d72544f9527533509fc1e5207e859b921
manufebie <manu.febie@gmail.com>
2018-09-26 22:42:40 +0700

    Code is somewhat finished. Still have bugs. But I am too tired to fix them.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ## Assignment
-## Ceasar Cypher Encoder / Decoder
+# Ceasar Cypher Encoder / Decoder
 ### About
+
+Computer Science assignment. Objective is using the Ceasar Cypher to encrypt messages based on their given rotation (shift).
+
+
 
--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -1,10 +1,41 @@
-# Caesar Cipher
+'''
+Ceasar Cypher
+Encrypt messages by shifting the letters
+Had some help using example given on Sololearn: https://code.sololearn.com/cK267fizDS8H/#py
+'''
 import string
 
-# set alphabet
-ab_lower = string.ascii_lowercase
-ab_upper = string.ascii_uppercase
-shift = int(input('Encrypt with shift: '))
 
-print(ab_lower)
-print(ab_upper)
+plain = string.ascii_letters # alphabet
+shift = int(input('Shift by: ')) # Enter the rotation 
+message = input('Encrypt: ') # Message user want's to ecrypt
+print() 
+
+
+def encrypt_message(shift, message): # function that generates the encrypted message based on shift input
+    encrypt_message = ''
+
+    for letter in message:
+        i = (plain.index(letter) + shift) % 52
+        encrypt_message += plain[i] # set encrypted message equal to the rotation
+
+    return 'Encrypted message: {}'.format(encrypt_message)
+
+
+def decrypt_message(shift, encrypt): # function decrypts the encrypted message
+    decrypted_message = ''
+
+    for letter in encrypt:
+        try:
+            i = (plain.index(letter) - shift) % 52
+            decrypted_message += plain[i]
+        except ValueError: # added Value error because there was a value error
+            decrypted_message += letter
+
+    return 'Decrypted: {}'.format(decrypted_message)
+
+encrypted = encrypt_message(shift, message)
+print(encrypted)
+
+decrypted = decrypt_message(shift, encrypted)
+print(decrypted)
